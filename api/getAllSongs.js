13a21bb4fc1ecef0a7e5430b7dac159e3cea7097
ckaeda <iamgpmc@gmail.com>
2025-11-06 17:13:15 +0800

    reworked getAllSongs to only call event title when event needed

--- api/getAllSongs.js
+++ api/getAllSongs.js
@@ -5,22 +5,28 @@ export default async function handler(req, res) {
             cache: "no-cache",
             headers: { 'x-apikey': process.env.DB_API_KEY }
         });
-        const event_response = await fetch('https://rnbandrepo-e7c5.restdb.io/rest/event?max=1', {
-            method: "GET",
-            cache: "no-cache",
-            headers: { 'x-apikey': process.env.DB_API_KEY }
-        });
+
         if (!songs_response.ok) {
             throw new Error(`HTTP error! status: ${songs_response.status}`);
         }
         const songs_data = await songs_response.json();
 
-        if (!event_response.ok) {
-            throw new Error(`HTTP error! status: ${event_response.status}`);
-        }
-        const event_data = await event_response.json();
+        var event_title = "";
+        if (songs_data.filter(song => song.event).length > 0) {
+            const event_response = await fetch('https://rnbandrepo-e7c5.restdb.io/rest/event?max=1', {
+                method: "GET",
+                cache: "no-cache",
+                headers: { 'x-apikey': process.env.DB_API_KEY }
+            });
 
-        return res.status(200).json({ event: event_data[0].title, songs: songs_data });
+            if (!event_response.ok) {
+                throw new Error(`HTTP error! status: ${event_response.status}`);
+            }
+            const event_data = await event_response.json();
+            event_title = event_data[0].title;
+        }
+
+        return res.status(200).json({ event: event_title, songs: songs_data });
     } catch (error) {
         console.error('Error:', error);
         return res.status(400).json({ "message": "An error has occurred" });
