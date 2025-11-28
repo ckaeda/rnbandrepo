f3f265cd22d029e758c4c262306d2b53d5e6c5e4
ckaeda <iamgpmc@gmail.com>
2025-11-28 16:58:01 +0800

    removed toHeader in parseSong

--- src/functions/parseSong.js
+++ src/functions/parseSong.js
@@ -1,40 +1,6 @@
 import ChordSheetJS, { Chord } from 'chordsheetjs';
 import { convertToNumeral } from './convertToNumeral';
 
-const toHeader = [
-    'Intro',
-    'Pre-Chorus',
-    'Pre-Chorus 1',
-    'Pre-Chorus 2',
-    'Chorus 1',
-    'Chorus 2',
-    'Chorus 3',
-    'Bridge',
-    'Bridge 1',
-    'Bridge 2',
-    'Bridge 3',
-    'Bridge 4',
-    'Outro',
-    'Instrumental',
-    'Instrumental 1',
-    'Instrumental 2',
-    'Post-Chorus',
-    'Post-Chorus 2',
-    "Refrain",
-    'Interlude',
-    'Interlude 2',
-    'Vamp',
-    'Vamp 2',
-    'Vamp 3',
-    'Tag',
-    'Tag 1',
-    'Tag 2',
-    'Breakdown',
-    'Turnaround',
-    'Hook',
-    'Coda',
-    'Ending'
-];
 
 export function parseSong(metadata, lyrics, key, keyDiff, numeralMode = false) {
     const origKey = metadata.defaults.Orig;
@@ -64,10 +30,8 @@ export function parseSong(metadata, lyrics, key, keyDiff, numeralMode = false) {
 
     const formatter = new ChordSheetJS.HtmlTableFormatter();
     var disp = formatter.format(unserializedSong);
-
-    for (let str in toHeader) {
-        disp = disp.replaceAll('<td class="comment">' + toHeader[str] + '</td>', '<h3 class="label">' + toHeader[str] + '</h3>');
-    }
+        
+    disp = disp.replace(/<td class="comment">(.*?)<\/td>/g, '<h3 class="label">$1</h3>');
 
     return disp;
 }
