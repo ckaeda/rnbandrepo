3a3103d09cd73985250b227e64a099c720e06822
ckaeda <iamgpmc@gmail.com>
2025-11-28 17:14:02 +0800

    optimized numeral conversion

--- src/functions/parseSong.js
+++ src/functions/parseSong.js
@@ -1,5 +1,4 @@
 import ChordSheetJS, { Chord } from 'chordsheetjs';
-import { convertToNumeral } from './convertToNumeral';
 
 
 export function parseSong(metadata, lyrics, key, keyDiff, numeralMode = false) {
@@ -8,10 +7,7 @@ export function parseSong(metadata, lyrics, key, keyDiff, numeralMode = false) {
     const parser = new ChordSheetJS.UltimateGuitarParser();
     const unserializedSong = parser.parse(`${lyrics}`).setKey(origKey).changeKey(key).transpose(keyDiff);
 
-    if (numeralMode) {
-        convertToNumeral(unserializedSong, Chord.parse(key).transpose(keyDiff).toString());
-    }
-
+    const currentKey = Chord.parse(key).transpose(keyDiff).toString();
     for (let i = 0; i < unserializedSong.lines.length; i++) {
         const line = unserializedSong.lines[i];
         if (!line.items) continue;
@@ -20,17 +16,27 @@ export function parseSong(metadata, lyrics, key, keyDiff, numeralMode = false) {
             const item = line.items[j];
             if (!item.chords || item.chords.trim() == "") continue;
 
-            if (["C#", "Db", "D#", "Eb", "F#", "Gb", "G#", "Ab", "A#", "Bb"].includes(Chord.parse(key).transpose(keyDiff).toString())) {
-                item.chords = Chord.parse(item.chords).useModifier('b').normalize();
+            if (["C#", "Db", "D#", "Eb", "F#", "Gb", "G#", "Ab", "A#", "Bb"].includes(currentKey)) {
+                item.chords = Chord.parse(item.chords).useModifier('b').normalize().toString();
             } else {
-                item.chords = Chord.parse(item.chords).useModifier('#').normalize();
+                item.chords = Chord.parse(item.chords).useModifier('#').normalize().toString();
+            }
+
+            if (numeralMode) {
+                item.chords = Chord.parse(item.chords)
+                    .toNumeralString(currentKey)
+                    .replace(/([ivx]+)m/g, '$1')
+                    .replace('#vi', 'vi')
+                    .replace('bvii', 'vi')
+                    .replace('#iii', 'iii')
+                    .replace('#VI', 'bVII')
             }
         }
     }
 
     const formatter = new ChordSheetJS.HtmlTableFormatter();
     var disp = formatter.format(unserializedSong);
-        
+
     disp = disp.replace(/<td class="comment">(.*?)<\/td>/g, '<h3 class="label">$1</h3>');
 
     return disp;
